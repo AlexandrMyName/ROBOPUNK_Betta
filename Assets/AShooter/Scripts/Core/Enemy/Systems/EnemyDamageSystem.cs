eb768f9d90f7cf0d86a33402e03d0cd75454742e
YanglyaevRamil <32882454+YanglyaevRamil@users.noreply.github.com>
2023-08-27 23:43:24 +0300

    Add health enemy
    added enemy health component to Enemy damage system

--- Assets/AShooter/Scripts/Core/Enemy/Systems/EnemyDamageSystem.cs
+++ Assets/AShooter/Scripts/Core/Enemy/Systems/EnemyDamageSystem.cs
@@ -1,4 +1,5 @@
 using abstracts;
+using User;
 using UnityEngine;
 
 namespace Core
@@ -6,15 +7,22 @@ namespace Core
     public class EnemyDamageSystem : BaseSystem
     {
         private IGameComponents _components;
+        private IHealth _health;
 
         protected override void Awake(IGameComponents components)
         {
             _components = components;
+            _health = _components.BaseObject.GetComponent<IHealth>();
         }
 
         protected override void Update()
         {
 
+            if (_health != null && _health.CurrentHealth <= 0)
+            {
+                Debug.LogWarning($"Death of the enemy  {_components.BaseObject.name}");
+                Object.Destroy(_components.BaseObject);
+            }
         }
     }
 }
