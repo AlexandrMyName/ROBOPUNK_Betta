2341cda92497a02168b96674454494a33f65a4bb
YanglyaevRamil <32882454+YanglyaevRamil@users.noreply.github.com>
2023-09-07 22:23:16 +0300

    add logic the enemy stands when attacking in melee

--- Assets/AShooter/Scripts/Core/Enemy/Systems/EnemyMovementSystem.cs
+++ Assets/AShooter/Scripts/Core/Enemy/Systems/EnemyMovementSystem.cs
@@ -1,19 +1,27 @@
-using Abstracts;
-using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.AI;
+using UnityEngine;
+using Abstracts;
+using System;
+using UniRx;
 
 namespace Core
 {
 
     public class EnemyMovementSystem : BaseSystem
     {
-
+        private List<IDisposable> _disposables = new();
         private NavMeshAgent _navMeshAgent;
         private Transform _targetTransform;
+        private bool _readyToMeleeAttack;
+
 
         protected override void Awake(IGameComponents components)
         {
-            _targetTransform = components.BaseTransform.GetComponent<Enemy>().playerTransform;
+            var enemy = components.BaseObject.GetComponent<Enemy>();
+            _targetTransform = enemy.playerTransform;
+            enemy.IsReadyToMeleeAttack.Subscribe(OnReadyToMeleeAttack);
+
             _navMeshAgent = components.BaseObject.GetComponent<NavMeshAgent>();
 
 #if UNITY_EDITOR
@@ -34,17 +42,38 @@ namespace Core
 
         protected override void Update()
         {
-            Moving(_targetTransform.position);
-            //_navMeshAgent.isStopped = true;
+            if (_readyToMeleeAttack)
+                Stay();
+            else 
+                Moving(_targetTransform.position);
         }
 
+        private void Stay()
+        {
+            _navMeshAgent.isStopped = true;
+        }
 
         public void Moving(Vector3 targetPosition)
         {
+            if (_navMeshAgent.isStopped)
+                _navMeshAgent.isStopped = false;
+
             if (_navMeshAgent.isActiveAndEnabled)
                 _navMeshAgent.SetDestination(targetPosition);
         }
 
+
+        private void Dispose()
+        {
+            _disposables.ForEach(d => d.Dispose());
+            _disposables.Clear();
+        }
+
+
+        private void OnReadyToMeleeAttack(bool val)
+        {
+            _readyToMeleeAttack = val;
+        }
 
     }
 
