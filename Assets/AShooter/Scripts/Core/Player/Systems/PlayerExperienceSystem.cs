d3aa2b272df82b6633ad805279fe76dac8ce72d4
YanglyaevRamil <32882454+YanglyaevRamil@users.noreply.github.com>
2023-10-12 08:38:03 +0300

    Add LvL

--- Assets/AShooter/Scripts/Core/Player/Systems/PlayerExperienceSystem.cs
+++ Assets/AShooter/Scripts/Core/Player/Systems/PlayerExperienceSystem.cs
@@ -2,7 +2,7 @@ using Abstracts;
 using System;
 using System.Collections.Generic;
 using UniRx;
-
+using UnityEngine;
 
 namespace Core
 {
@@ -10,33 +10,75 @@ namespace Core
     public class PlayerExperienceSystem : BaseSystem , IDisposable
     {
         
-        private List<IDisposable> _disposables = new();
+        private readonly List<IDisposable> _disposables = new List<IDisposable>();
         private IExperienceView _experienceView;
+        private IExperienceHandle _experienceHandle;
+
+        private float requiredExperienceForNextLevel = 10f;
+        private float progressRate = 2.0f;
 
 
         protected override void Awake(IGameComponents components)
         {
-            _disposables = new();
-
             _experienceView = components.BaseObject.GetComponent<IPlayer>().ComponentsStore.Views.Experience;
+            _experienceHandle = components.BaseObject.GetComponent<IPlayer>().ComponentsStore.ExperienceHandle;
 
-            var currentExperience = components.BaseObject.GetComponent<IPlayer>().ComponentsStore.ExperienceHandle.CurrentExperience;
-
-            currentExperience.Subscribe(UpdateDisplay);
+            SubscribeToExperienceChanges();
+            SubscribeToLevelChanges();
 
             _experienceView.Show();
-
-            _experienceView.ChangeDisplay(currentExperience.Value);
+            UpdateDisplay(_experienceHandle.CurrentExperience.Value, _experienceHandle.CurrentLevel.Value, requiredExperienceForNextLevel);
         }
 
 
-        private void UpdateDisplay(float value)
+        private void SubscribeToExperienceChanges()
         {
-            _experienceView.ChangeDisplay(value);
+            _disposables.Add(_experienceHandle.CurrentExperience.Subscribe(OnExperienceChanged));
         }
 
 
-        public void Dispose() => _disposables.ForEach(disposable => disposable.Dispose());
+        private void SubscribeToLevelChanges()
+        {
+            _disposables.Add(_experienceHandle.CurrentLevel.Subscribe(OnLevelChanged));
+        }
+
+
+        private void OnLevelChanged(int valueLevel)
+        {
+            UpdateDisplay(_experienceHandle.CurrentExperience.Value, valueLevel, requiredExperienceForNextLevel);
+        }
+
+
+        private void OnExperienceChanged(float valueExperience)
+        {
+            if (valueExperience >= requiredExperienceForNextLevel)
+            {
+                MakeLevelUp();
+
+                requiredExperienceForNextLevel = requiredExperienceForNextLevel * progressRate;
+            }
+
+            UpdateDisplay(valueExperience, _experienceHandle.CurrentLevel.Value, requiredExperienceForNextLevel);
+        }
+
+
+        private void MakeLevelUp()
+        {
+            _experienceHandle.CurrentLevel.Value++;
+        }
+
+
+        private void UpdateDisplay(float valueCurrentExperience, int valueLvl, float valueProgressExperience)
+        {
+            _experienceView.ChangeDisplay(valueCurrentExperience, valueLvl, valueProgressExperience);
+        }
+
+
+        public void Dispose() 
+        {
+            _disposables.ForEach(disposable => disposable.Dispose());
+            _disposables.Clear();
+        } 
 
 
     }
